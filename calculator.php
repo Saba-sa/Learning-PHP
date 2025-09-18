b80dbee891d57279f9fe53a79e129e91ebe78d44
Saba-sa <sabaali3201@gmail.com>
2025-09-18 12:50:50 +0500

    Fix form action and sanitize input in calculator.php

--- calculator.php
+++ calculator.php
@@ -9,7 +9,7 @@
 
 <body>
 
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+    <form action="<?php echo htmlspecialchars(string: $_SERVER['PHP_SELF']);?>" method="post">
 
         <input type="number" name="num1" id="num1">
         <select name="operator" id="operator">
@@ -23,13 +23,52 @@
         <button>calculate result</button>
 
     </form>
-    if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $num1=htmlentities($_POST['num1'])
-    $num2=htmlentities($_POST['num2'])
-    $operator=htmlentities($_POST['operator'])
 
-    }
     <?php
+     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $num1 = filter_input(INPUT_POST, "num1", FILTER_SANITIZE_NUMBER_FLOAT);
+        $num2 = filter_input(INPUT_POST, "num2", FILTER_SANITIZE_NUMBER_FLOAT);
+
+        $operator = filter_input(INPUT_POST,  "operator",   FILTER_SANITIZE_SPECIAL_CHARS);
+        $error = false;
+        if (empty($num1) || empty($num2) || empty($operator)) {
+            echo "<p>Both numbers are required</p>";
+            $error = true;
+        }
+        if (!is_numeric($num1) || !is_numeric($num2)) {
+            echo "<p>Both values must be numeric</p>";
+            $error = true;
+        }
+
+        if (!$error) {
+            $value = 0;
+
+            switch ($operator) {
+                case "+":
+                    $value = $num1 + $num2;
+                    break;
+                case "-":
+                    $value = $num1 - $num2;
+                    break;
+                case "*":
+                    $value = $num1 * $num2;
+                    break;
+                case "/":
+                    $value = $num1 / $num2;
+                    break;
+                case "%":
+                    $value = $num1 % $num2;
+                    break;
+                default:
+                    echo "<p>Invalid operator</p>";
+                    break;
+
+            }
+            echo "<h1>Result: $value</h1>";
+
+
+        }
+    }
     
     ?>
 
